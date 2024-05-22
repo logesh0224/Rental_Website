e7e624f3cdbc527e43701eb1c236898137fd7559
LOGESHBOOPATHI S <110893174+kittyboyy@users.noreply.github.com>
2024-05-23 02:01:51 +0530

    Added Seller Details

--- backend/src/models/house.ts
+++ backend/src/models/house.ts
@@ -29,7 +29,7 @@ const hotelSchema = new mongoose.Schema<HotelType>({
   adultCount: { type: Number, required: true },
   facilities:[{type:String,required:true}],
   childCount: { type: Number, required: true },
-  pricePerMonth: { type: Number, required: true },
+  pricePerNight: { type: Number, required: true },
   starRating: { type: Number, required: true, min: 1, max: 5 },
   imageUrls: [{ type: String, required: true }],
   lastUpdated: { type: Date, required: true },
